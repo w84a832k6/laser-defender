6d82b9cbef925c0f808281256eae5b54c42cf67a
andy <w84a832k6@gmail.com>
2018-01-24 09:37:29 +0800

    add .

---
+++ Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public GameObject projectile;
+    public float projectilespeed = 5f;
+    public float health = 150;
+
+    void Update()
+    {
+        Vector3 startPosition = transform.position + new Vector3(0f, -1f, 0f);
+        GameObject missile = Instantiate(projectile, startPosition, Quaternion.identity) as GameObject;
+        missile.GetComponent<Rigidbody2D>().velocity = new Vector3(0f, -projectilespeed, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Projectile missile = collision.gameObject.GetComponent<Projectile>();
+
+        if(missile)
+        {
+            health -= missile.GetDamage();
+            missile.Hit();
+            if(health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+
+}
---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+    public float speed = 2.0f;
+    public float padding = 1f;
+    public GameObject projectile;
+    public float projectileSpeed;
+    public float firingRate = 0.2f;
+    public float health = 250f;
+
+    float xmin;
+    float xmax;
+    
+
+	// Use this for initialization
+	void Start () {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f, distance));
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
+        Debug.Log(leftmost);
+        Debug.Log(rightmost);
+    }
+
+    void Fire()
+    {
+        GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0f, projectileSpeed, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating("Fire", 0.0001f, firingRate);
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke("Fire");
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow)){
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        //restrict the player to the gamespoace
+        float newx = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newx, transform.position.y, transform.position.z);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Projectile missile = collision.gameObject.GetComponent<Projectile>();
+
+        if (missile)
+        {
+            health -= missile.GetDamage();
+            missile.Hit();
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
